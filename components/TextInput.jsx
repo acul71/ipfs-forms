a1badbbdab30cabde833ea4675849dba41894902
acul71 <luca.pisani@birdo.net>
2021-08-04 01:27:32 +0200

    start working on Text Input component

---
+++ components/TextInput.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function TextInput(props) {
+  return (
+    
+  )
+}
+
+TextInput.prototype = {
+  isRequired: PropTypes.bool.isRequired,
+
+}
+
+export default TextInput
