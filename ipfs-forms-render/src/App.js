3b62883cdaee2fe8b76c6011e11b5d59fb0b3215
acul71 <luca.pisani@birdo.net>
2021-08-15 19:10:30 +0200

    Working on encrypting and saving the form on web3.storage

--- ipfs-forms-render/src/App.js
+++ ipfs-forms-render/src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState}  from 'react'
+import React, {useRef, useState, useEffect}  from 'react'
 
 //import './App.css';
 //import 'bootstrap/dist/css/boostrap.min.css'
@@ -11,24 +11,38 @@ import Button from 'react-bootstrap/Button'
 import FormTitle from './components/FormTitle'
 import TextInput from './components/TextInput'
 
-import {passwordGenerator, aes256, mytest} from './lib/formCrypto'
+import {formEncrypt} from './lib/formCrypto'
+import storeFiles from './lib/formWeb3Storage'
+
 
 //const formCrypto = require('./lib/formCrypto')
+
+import axios from 'axios'
 
 function App() {
   let submitErrorMsg = React.createRef()
   
-  const [formSubmitted, setFormSubmitted] = useState(false)
+  //const [formSubmitted, setFormSubmitted] = useState(false)
+  // formState: 0=loading 1=to fill 2=submitted
+  const [formState, setFormState] = useState(0)
+  const [form, setForm] = useState('')
+  const [questRes, setQuestRes] = useState([])
+  const [composerPublicKey, setComposerPublicKey] = useState('')
+
 
   
 
-  function handleSubmit() {
-    console.log('questRes=', questRes)
+  async function handleSubmit() {
+    //console.log('questRes=', questRes)
     
-    for (let i = 0; i < questRef.length; i++) {
+    console.log('questRef=', questRef)
+
+    //for (let i = 0; i < questRef.length; i++) {
+    for (let i = 0; i < form.quest.length; i++) {
       questRef[i].current.testInput()  
     }
     
+    console.log('handleSubmit: questRes=', questRes)
 
     if (questRes.map(i => i.res).every(Boolean) === true) {
       // Handle submit
@@ -39,6 +53,8 @@ function App() {
       //
       // Get form data
       const formData = questRes.map( (elem) => elem.val )
+
+      /*
       // Generate random password
       const aesKey= passwordGenerator()
       console.log('password=', aesKey)
@@ -47,20 +63,24 @@ function App() {
       const encryptedFormData = aes256.encrypt( aesKey, JSON.stringify(formData) )
       // Encrypt aesKey (TODO)
       const composerEncryptedKey = aesKey
-      
-      const formRes = {
-        formId: formId,
-        composerEncryptedKey: composerEncryptedKey,
-        formData: encryptedFormData,
-      }
-      
+      */
+
+      //const [composerEncryptedKey, encryptedFormData ] = await formEncrypt(composerPublicKey, formData)
+
+
+      const formRes = await formEncrypt(composerPublicKey, formData)
+      const formResString = JSON.stringify(formRes)
+
+      // Save formRes to web3.storage
+      const cid = await storeFiles(formResString, 'FormTest.json')
+
       console.log('formRes=', formRes)
       // Decrypt Test
-      console.log('decrypted form data=', aes256.decrypt(aesKey, formRes.formData))
-
-      mytest()
+      //console.log('decrypted form data=', aes256.decrypt(aesKey, formRes.formData))
       
-      setFormSubmitted(true)
+      
+      
+      setFormState(2)
       
     } else {
       console.log("Form rejected!")
@@ -69,6 +89,12 @@ function App() {
 
   }
 
+  
+
+  
+
+
+  /*
   const formId = 'form001'
   const form = {
     title: "Form Title",
@@ -95,44 +121,120 @@ function App() {
       },
     ]
   }
+  */
 
-  const numOfQuest = form.quest.length
-  // questRes (arr of val,bool) contains all values and "check ok" for questions
-  const questRes = [ ...Array(numOfQuest).keys() ].map( i => i = {val: '', res: false})
-  //console.log("questRes=", questRes)
 
+  
+  // questComp is the question component parsed from json Form
+  let questComp
+    
+  const formUrl = 'https://luca.files.fission.name/p/Form/FORM_1628993404351.json'
+    
+  //let questRes
+  /*
+  if (form === '') {
+    questRes = getForm(formUrl)
+    console.log('questRes after getForm=', questRes)
+  }
+  */
+  // To be fixed (the number 100 is arbitrary. It should be set to form.quest.length But I have problems with useRef() )
+  const questRef = new Array(100)
+  
+  //for(var i = 0; i < questRef.length; i++){
+  // To be fixed
+  // Disabling eslint hook check: It should be safe in this case (binding questRef array istances to useRef)
+  /* eslint-disable */
+  for(var i = 0; i < 100; i++){
+    questRef[i] = useRef()
+  }
+  /* eslint-enable */
+  
+  /*
   function setQuestRes(idx, val, state) {
     questRes[idx] = {val: val, res: state}
     //questRes[idx].val = val
     //questRes[idx].res = state
   }
-  
-  // questComp is the question component parsed from json Form
-  let questComp
-  const questRef = new Array(numOfQuest)
-  
-  // Disabling eslint hook check: It should be safe in this case (binding questRef array istances to useRef)
-  /* eslint-disable */
-  for(var i = 0; i < questRef.length; i++){
-    questRef[i] = useRef()
-  }
-  /* eslint-enable */
-  
+  */
 
+  useEffect( () => {
+
+    // Get composer public Key
+    async function getComposerPublicKey(composerPublicKeyUrl='') {
+      try {
+        const composerPublicKeyFetch = await axios.get(composerPublicKeyUrl)
+        console.log('getComposerPublicKey: composerPublicKey=', composerPublicKeyFetch.data)
+        const composerPublicKey = composerPublicKeyFetch.data
+        return composerPublicKey
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    // Get form Json
+    async function getForm(formUrl='') {
+      try {
+        const formFetch = await axios.get(formUrl)
+        console.log('getForm: form=', formFetch.data)
+        const formData = formFetch.data
+
+        const numOfQuest = formData.quest.length
+        // questRes (arr of val,bool) contains all values and "check ok" for questions
+        const wquestRes = [ ...Array(numOfQuest).keys() ].map( i => i = {val: '', res: false})
+        //console.log("questRes=", questRes)
+
+        // https://luca.files.fission.name/p/Form/publicReadKey.rsa
+        const composerPublicKeyUrl = 'https://' + formData.composerUsername + '.files.fission.name/p/Form/publicReadKey.rsa'
+        setComposerPublicKey( await getComposerPublicKey( composerPublicKeyUrl ) )
+        setForm(formData)
+        setFormState(1)
+        setQuestRes(wquestRes)
+        //return questRes
+      } catch (error) {
+        console.error(error)
+      }
+
+    }
+    
+
+    if (form === '') {
+      // componentDidMount
+      console.log("componentDidMount")
+      getForm(formUrl)
+
+    } else {
+      // componentDidUpdate
+      console.log("componentDidUpdate")
+    }
+  }, [form])
+
+
+
+
+
+
+  if (formState === 0) {
+    return (
+      <Container fluid="sm">
+        <p>Loading....</p>
+      </Container>  
+    )
+  }
+  // else
   return (
     <Container fluid="sm">
-      { formSubmitted ? 
+      { formState === 2 ? 
         <p>{form.formSubmittionMessage}</p>
       :
         <Form onSubmit={(e) => e.preventDefault()}>
           <FormTitle title={form.title} />
           {
             form.quest.map((quest,idx) => {
-              console.log("quest=", quest)
+              console.log("quest=", quest, questRes)
               
               switch (quest.type) {
                 case 'text':
-                  questComp = <TextInput key={idx} label={quest.label} placeHolder={quest.placeHolder} isRequired={quest.isRequired} idx={idx} setQuestRes={setQuestRes} ref={questRef[idx]}/> 
+                  questComp = <TextInput key={idx} label={quest.label} placeHolder={quest.placeHolder} isRequired={quest.isRequired} idx={idx} questRes={questRes} setQuestRes={setQuestRes} ref={questRef[idx]}/> 
                   break;
                 default:
                   break;
